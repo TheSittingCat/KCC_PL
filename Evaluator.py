2bd8ef68d229fb04bc4b4103c08c8fdf1c4675e5
Kave Eskandari <39281449+TheSittingCat@users.noreply.github.com>
2023-03-27 14:31:13 -0400

    Evaluator Development
    Added multiple functionalities to the evaluator.

--- Evaluator.py
+++ Evaluator.py
@@ -26,17 +26,85 @@ class Evaluator(Transformer):
     def shape_rule(self, args):
         return "shape"
     def lb(self, args):
-        return ":\t"
+        return ": \n \t"
     def rb(self, args):
         return "\n"
     def NUMBER(self, args):
         return float(args)
+    # Expression paths, the order of the methods is important. Returns the number of expression is a number, recursively calls the next expression if it is not a number.
     def exp(self, args):
+        if len(args) == 1:
+            return args[0]
+        else :
+            arg_list = []
+            for arg in args:
+                arg_list.append(str(arg))
+            args = " ".join(arg_list)
+            return args
+    def exp1(self, args):
+        if len(args) == 1:
+            return args[0]
+        else :
+            arg_list = []
+            for arg in args:
+                arg_list.append(str(arg))
+            args = " ".join(arg_list)
+            return args
+    def exp2(self, args):
+        if len(args) == 1:
+            return args[0]
+        else :
+            arg_list = []
+            for arg in args:
+                arg_list.append(str(arg))
+            args = " ".join(arg_list)
+            return args
+    def exp3(self, args):
+        if len(args) == 1:
+            return args[0]
+        else :
+            arg_list = []
+            for arg in args:
+                arg_list.append(str(arg))
+            args = " ".join(arg_list)
+            return args
+    def equals(self, args):
+        return "="
+    def plus(self, args):
+        return "+"
+    def minus(self, args):  
+        return "-"
+    def multiply(self, args):
+        return "*"
+    def divide(self, args):
+        return "/"
+    def modulus(self, args):
+        return "%"
+    def power(self, args):
+        return "**"
+    def STRING(self, args):
         return str(args)
-    
-test = "function test (int) { 1 }"
-grammar = Parser.grammar()
-parser = Parser.parser(grammar, "start")
-tree = Parser.tree_generator(parser, test)
-print(tree)
-print(Evaluator().transform(tree)[1:-1])
+    def expstr(self, args):
+        if len(args) == 1:
+            return args[0]
+        else :
+            arg_list = []
+            for arg in args:
+                arg_list.append(str(arg))
+            args = " ".join(arg_list)
+            return args
+    def end(self, args):
+        return ";"
+test = r'''function test (int) {
+Hello = "Hello World";
+Christina = 5;
+Camille = 6;
+ }'''
+
+def transform_result(entry):
+    grammar = Parser.grammar()
+    parser = Parser.parser(grammar, "start")
+    tree = Parser.tree_generator(parser, entry)
+    print(tree.pretty())
+    return Evaluator().transform(tree)[1:-1]
+transform_result(test)
